a5d844f979ef67eecf3ad525c982b853f4bd2734
Colin Kennedy <colin.kennedy@mesh.xyz>
2023-11-29 10:40:07 -0400

    fix title, head metadata

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: 'Home - Simple',
-  description: 'Page description',
+  title: 'InterWallet',
+  description: 'A next-generation crypto wallet for the next-generation of crypto adoption.',
 }
 
 import Hero from '@/components/hero'
