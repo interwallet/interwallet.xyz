747880ba6559129a6b0d2ef85ba5e4a59a900321
Colin Kennedy <colin.kennedy@mesh.xyz>
2023-11-29 01:46:38 -0400

    add email collection via firebase

--- components/newsletter.tsx
+++ components/newsletter.tsx
@@ -1,4 +1,37 @@
+'use client'
+// Newsletter.tsx
+import { initializeApp } from 'firebase/app';
+import { addDoc, collection, getFirestore } from 'firebase/firestore';
+import React from 'react';
+
 export default function Newsletter() {
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyA8ibH6-pWhpOMYQA7mNl_ji0jwSg4ib-8",
+    authDomain: "interwallet-xyz.firebaseapp.com",
+    projectId: "interwallet-xyz",
+    storageBucket: "interwallet-xyz.appspot.com",
+    messagingSenderId: "655516345279",
+    appId: "1:655516345279:web:72b0cebf09559ab0b5370a",
+    measurementId: "G-7PFNH0QQ0J"
+  };
+  
+  const fbApp = initializeApp(firebaseConfig);
+  const db = getFirestore(fbApp);
+  
+  async function addData() {
+    console.log("Adding data");
+    try {
+      const docRef = await addDoc(collection(db, "emails"), {
+        email: "johndoe@example.com",
+        time: new Date()
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (error) {
+      console.error("Error adding document: ", error);
+    }
+  }
+
   return (
     <section>
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
@@ -46,7 +79,7 @@ export default function Newsletter() {
                 <form className="w-full lg:w-auto">
                   <div className="flex flex-col sm:flex-row justify-center max-w-xs mx-auto sm:max-w-md lg:mx-0">
                     <input type="email" className="form-input w-full appearance-none bg-gray-800 border border-gray-700 focus:border-gray-600 rounded-sm px-4 py-3 mb-2 sm:mb-0 sm:mr-2 text-white placeholder-gray-500" placeholder="Your email…" aria-label="Your email…" />
-                    <a className="btn text-white bg-blue-600 hover:bg-blue-700 shadow" href="#0">Subscribe</a>
+                    <a className="btn text-white bg-blue-600 hover:bg-blue-700 shadow" href="#0" onClick={addData}>Subscribe</a>
                   </div>
                   {/* Success message */}
                   {/* <p className="text-sm text-gray-400 mt-3">Thanks for subscribing!</p> */}
