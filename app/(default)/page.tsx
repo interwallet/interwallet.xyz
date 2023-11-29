bb1f558f7b593b984283ae4398afb9ee014d294a
Colin Kennedy <colin.kennedy@mesh.xyz>
2023-11-29 02:19:16 -0400

    remove unpopulated sections

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
     <>
       <Hero />
       <Features />
-      <FeaturesBlocks />
-      <Testimonials />
+      {/* <FeaturesBlocks /> */}
+      {/* <Testimonials />1 */}
       <Newsletter />
     </>
   )
