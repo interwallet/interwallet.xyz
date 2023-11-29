8fe5c88de8a595cec410ada24a21c8b7f735287d
Colin Kennedy <colin.kennedy@mesh.xyz>
2023-11-29 01:49:09 -0400

    remove unused GA / firebase

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -9,28 +9,6 @@ import FeaturesBlocks from '@/components/features-blocks'
 import Testimonials from '@/components/testimonials'
 import Newsletter from '@/components/newsletter'
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyA8ibH6-pWhpOMYQA7mNl_ji0jwSg4ib-8",
-  authDomain: "interwallet-xyz.firebaseapp.com",
-  projectId: "interwallet-xyz",
-  storageBucket: "interwallet-xyz.appspot.com",
-  messagingSenderId: "655516345279",
-  appId: "1:655516345279:web:72b0cebf09559ab0b5370a",
-  measurementId: "G-7PFNH0QQ0J"
-};
-
-// Initialize Firebase
-const fbApp = initializeApp(firebaseConfig);
-const fbAnalytics = getAnalytics(fbApp);
-
 export default function Home() {
   return (
     <>
